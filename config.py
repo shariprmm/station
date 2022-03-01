6ae5dc3ce0b3ff203f486bb03bc60ccfd3159402
shariprmm <46217575+shariprmm@users.noreply.github.com>
2022-03-01 20:30:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-login = "YANDEX_LOGIN"
-password = "YANDEX_PASSWORD"
-telegram_bot_token = "TG_BOT_TOKEN"
+login = "opel73rus"
+password = "89272723003Qw"
+telegram_bot_token = "5217320039:AAFhfGdAy2gI2Yj7UrdL9WXSsn8dru9fLDI"
 
 # To disable proxy: proxy = None
 proxy = { 
-    'proxy_url': 'http://host:port',
+    'proxy_url': 'http://178.130.86.81:8080',
 }
